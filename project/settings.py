ee1cb05e76857aaa9592344110e8fc64f7d0ae48
Roberto Duarte <103781917+robertoduartejr@users.noreply.github.com>
2022-09-21 01:13:22 -0300

    API finalizada. Preparando Heroku v2

--- project/settings.py
+++ project/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jcw4htf_qq)%kivj7y-!jz%fkh2)(bu_6+%g^i@4&f**zw#_d9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
